ba3af18892b4c3ee75d46bb5398eb89f0e45fc4c
Arly <mrsmileball@gmail.com>
2018-12-26 20:15:57 +0200

    Adding slow autoscroll in main menu, adding modul window, fix bugs

--- js/slowjakor.js
+++ js/slowjakor.js
@@ -6,3 +6,11 @@ $(document).ready(function(){
         $('body,html').animate({scrollTop: top}, 1000);
     });
 });
+$(document).ready(function(){
+    $(".menu__item").on("click", "a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
